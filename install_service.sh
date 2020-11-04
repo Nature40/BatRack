86ff58ca1cc754d23cca3c567af0ccadd20ad42b
Patrick Lampe <lampep@informatik.uni-marburg.de>
2020-11-04 18:08:07 +0100

    change the service name to BatRack

--- install_service.sh
+++ install_service.sh
@@ -4,5 +4,5 @@ cp start_bat_rack.sh /home/pi/start_bat_rack.sh
 chmod +x /home/pi/start_bat_rack.sh
 mkdir /home/pi/BatRack/data/
 sudo systemctl daemon-reload
-sudo systemctl enable BatRecorder.service
-sudo systemctl start BatRecorder
+sudo systemctl enable BatRack.service
+sudo systemctl start BatRack
