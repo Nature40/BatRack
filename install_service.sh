0fe8905bb65c8df85453963f24dc8312473272d7
Patrick Lampe <lampep@informatik.uni-marburg.de>
2020-11-05 17:05:33 +0100

    edit the service to use only the folder of the batrack

--- install_service.sh
+++ install_service.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-cp BatRack.service /etc/systemd/system/BatRack.service
-cp start_bat_rack.sh /home/pi/start_bat_rack.sh
-chmod +x /home/pi/start_bat_rack.sh
-mkdir /home/pi/BatRack/data/
+sudo cp BatRack.service /etc/systemd/system/BatRack.service
+chmod +x /home/pi/BatRack/start_bat_rack.sh
+sudo mkdir /home/pi/BatRack/data/
 sudo systemctl daemon-reload
 sudo systemctl enable BatRack.service
 sudo systemctl start BatRack
