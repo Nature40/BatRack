febded0df772eb55f8fe43367b7e3758ca1f7a62
Patrick Lampe <lampep@informatik.uni-marburg.de>
2020-11-05 17:22:20 +0100

    because every thing is now owned by root we have to use sudo to execute BatRack

--- start_bat_rack.sh
+++ start_bat_rack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/pi/RPi_Cam_Web_Interface
-./debug.sh&
+sudo ./debug.sh&
 cd /home/pi/BatRack/data
 now=`date +"%Y_%m_%d-%H:%M"`
-python3 ../BatRack.py > /home/pi/BatRack/data/$now.log 2>&1
+sudo python3 ../BatRack.py > /home/pi/BatRack/data/$now.log 2>&1
